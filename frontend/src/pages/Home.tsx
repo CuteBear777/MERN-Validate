19ee056f0d26fd70ccf047b1bf34c0e330e1c23e
CuteBear777 <184015708+CuteBear777@users.noreply.github.com>
2024-10-07 02:30:27 -0700

    Second Release with Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -44,6 +44,7 @@ const Home  = () => {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+            {/* Seconde Release Change part! */}
             {user?.role == 'admin' ? 
             (
                 <>
@@ -56,7 +57,7 @@ const Home  = () => {
                 <>
                     <Link to={`/modify/${user?._id}`}><p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Change your information!</p></Link>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                         You Logged in your profile page. You are able to change your information.
+                         You Logged in your profile page. You can change your information. 
                     </p>
                 </>
             )}
